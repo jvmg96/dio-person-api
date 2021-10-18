765515a4a60d81669c23b4c2749263eb182d2b56
João Victor <joaovictormguimaraes@gmail.com>
2021-10-17 21:33:43 -0300

    Project Person API

--- src/main/java/one/digitalinnovation/personapi/dto/request/PersonDTO.java
+++ src/main/java/one/digitalinnovation/personapi/dto/request/PersonDTO.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import one.digitalinnovation.personapi.entity.Phone;
+import org.hibernate.validator.constraints.br.CPF;
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -16,13 +20,21 @@ public class PersonDTO {
 
         private Long id;
 
+        @NotEmpty
+        @Size(min = 2, max = 100)
         private String firstName;
 
+        @NotEmpty
+        @Size(min = 2, max = 100)
         private String lastName;
 
+        @NotEmpty
+        @CPF
         private String cpf;
 
         private LocalDate birthDate;
 
+        @Valid
+        @NotEmpty
         private List<Phone> phones;
 }
--- src/main/java/one/digitalinnovation/personapi/dto/request/PhoneDTO.java
+++ src/main/java/one/digitalinnovation/personapi/dto/request/PhoneDTO.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import one.digitalinnovation.personapi.enums.PhoneType;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Data
 @Builder
@@ -19,6 +21,7 @@ public class PhoneDTO {
     @Enumerated(EnumType.STRING)
     private PhoneType type;
 
-    
+    @NotEmpty
+    @Size(min = 13, max = 14)
     private String number;
 }
